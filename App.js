2cd96a96a6bccb7ce3a60a02ab5b39ef877a3db2
Aleksander Truss <aleksander.truss@nordhealth.com>
2022-09-11 16:11:35 +0300

    Exercise complete

--- App.js
+++ App.js
@@ -1,10 +1,65 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList, TextInput } from 'react-native';
+
+
 
 export default function App() {
+
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+
+  const [total, setTotal] = useState(0);
+
+  const [data, setData] = useState([{key: "History"}]);
+ 
+  const addition = () => {
+    var result = parseInt(num1)+parseInt(num2)
+    setTotal(result);
+    setData([...data, { key: num1 + " + " + num2 + " = " + result }]);
+  }
+
+  const substraction = () => {
+    var result = parseInt(num1)-parseInt(num2)
+    setTotal(result);
+    setData([...data, { key: num1 + " - " + num2 + " = " + result }]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>Result: {total}</Text>
+      <TextInput
+        style={{width:200, borderColor: 'gray', borderWidth:1}}
+        onChangeText={num1 => setNum1(num1)}
+        value={num1}
+        numeric
+        keyboardType={'numeric'}
+      />
+      <TextInput
+        style={{width:200, borderColor: 'gray', borderWidth:1}}
+        onChangeText={num2 => setNum2(num2)}
+        value={num2}
+        numeric
+        keyboardType={'numeric'}
+      />
+
+      <View style={styles.button_row}>
+        <Button onPress={addition} title="+" />
+        <Button onPress={substraction} title="-" />
+      </View>
+
+      <View style={styles.history}>
+        <FlatList style={styles.list}
+          data={data}
+          renderItem={({ item }) =>
+            <View style={styles.item}>
+              <Text>{item.key}</Text>
+            </View>
+          }
+        />
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +72,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button_row: {
+    flexDirection: "row",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  history: {
+    flexDirection: "row",
+  }, 
+  item: {
+    alignItems: 'center',
+  }
 });
